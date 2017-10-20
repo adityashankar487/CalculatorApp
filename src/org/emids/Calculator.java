4d8bc1658c2375d415d330b412e63b40bfe4c13d
vbennur <vbennur@connecture.com>
2017-10-20 20:12:14 +0530

    third commit

--- src/org/emids/Calculator.java
+++ src/org/emids/Calculator.java
@@ -2,21 +2,17 @@ package org.emids;
 
 public class Calculator {
 
-	
-	public static void add(String numbers){
-		  String[] numbersArray = numbers.split(",");
-	        if (numbersArray.length > 2) {
-	            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
-	        }
-	        else {
-	            for (String number : numbersArray) {
-	                Integer.parseInt(number); 
-	            }
-	        }
-	    }
-	
-		
-		
-	}
-
-
+	public static int add(String numbers) {
+		String[] numbersArray = numbers.split(",");
+		if (numbersArray.length > 2) {
+			throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+		} else {
+			 for (String number : numbersArray) {
+		            if (!number.isEmpty()) {
+		                Integer.parseInt(number);
+		            }
+		        }
+		    }
+		    return 0; // Added return
+		}
+}
